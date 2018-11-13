f235f0595dd906cd856a9723b0ed91680825d9a7
TomidaiSub <dd1116k@gmail.com>
2018-11-13 13:53:21 +0900

    テストクラスtest.csを作成しました。確認出来たら削除してください。

---
+++ Map_Form/test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map_Form {
+    class test {
+        //テストクラスを作成しました。
+    }
+}
