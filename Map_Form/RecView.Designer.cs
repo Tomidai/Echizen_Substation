ce25d3940b638f5f82f1bcc2ce8ac5c4062e2cab
nonnori3 <ka43578@pf7.so-net.ne.jp>
2018-11-13 14:46:47 +0900

    メソッドPublicに変更と設定メニュー追加

---
+++ Map_Form/RecView.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Map_Form {
+    partial class RecView {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser2.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.ScrollBarsEnabled = false;
+            this.webBrowser2.Size = new System.Drawing.Size(1076, 753);
+            this.webBrowser2.TabIndex = 0;
+            // 
+            // RecView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 753);
+            this.Controls.Add(this.webBrowser2);
+            this.Name = "RecView";
+            this.Text = "レコーダー";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+            this.Load += new System.EventHandler(this.Form2_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+    }
+}
---
+++ Map_Form/RecView.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Map_Form {
+    public partial class RecView : Form {
+        String RecIP = "";
+
+        public RecView(Camera_Form Form1, String IP) {
+            InitializeComponent();
+            RecIP = IP;
+
+            String URI = "http://" + RecIP + "/cgi-bin/general-cgi/portalServer.cgi?command=get_page&p_name=AVPortalSetting&p_version=1&page_name=po_mainview.html";
+
+
+            //URI = "http://" + RecIP;
+            webBrowser2.Navigate(new Uri(URI));
+
+
+        }
+        private void Form2_Load(object sender, EventArgs e) {
+
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e) {
+
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e) {
+
+            Camera_Form.RecOn = 0;
+            Camera_Form.recButton.Image= global::Map_Form.Properties.Resources.cont_b0171;
+
+        }
+
+        private void Form2_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
